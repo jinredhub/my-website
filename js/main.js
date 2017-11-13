d6542d0530a6687f130b858ecfa4792c4bd0dedf
jinredhub <jin@nieblo.com>
2017-11-12 23:54:08 -0800

    finished title and icons

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(".resume").on("click", function(event){
+	// event.preventdefault();
+	console.log("clicked");
+	window.open("../resume/JinRedmond-Resume.pdf");
+});
+
+$(".linkedIn-btn").on("click", function(){
+	window.open("http://github.com/jinredhub");
+});
+
+$(".github-btn").on("click", function(){
+	window.open("http://www.linkedin.com/in/jin-redmond");
+});
