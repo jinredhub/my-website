d069c99ccaf756deaf7a79c7da02533daad6b932
jinredhub <jin@nieblo.com>
2017-11-15 12:09:35 -0800

    added anchor and email function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
 
-$(".resume").on("click", function(event){
-	// event.preventdefault();
-	console.log("clicked");
-	window.open("../resume/JinRedmond-Resume.pdf");
-});
+$("#e-mail").on("click", function(event){
+	console.log("email clicked");
+	// alertify.success("Message sent");
+	alert("Message sent");
+})
 
+// opens github page
 $(".linkedIn-btn").on("click", function(){
 	window.open("http://github.com/jinredhub");
 });
-
+// opens linked in page
 $(".github-btn").on("click", function(){
 	window.open("http://www.linkedin.com/in/jin-redmond");
 });
+
+// opens first portfolio page 
+$("#link-prtfolio1").on("click", function(){
+	window.open();
+});
+// opens second portfolio page
+$("#link-prtfolio2").on("click", function(){
+	window.open();
+});
+// opens third portfolio page
+$("#link-prtfolio3").on("click", function(){
+	window.open();
+});
+
