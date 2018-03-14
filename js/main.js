4702895a7323e583027a65a683744464a227c3c1
jinredhub <jin@nieblo.com>
2018-03-14 15:04:29 -0700

    added smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,31 +1,47 @@
+$(document).ready(function(){
 
-$("#e-mail").on("click", function(event){
-	console.log("email clicked");
-	
+	$("a").on("click", function(event){
+		if(this.hash !== ""){
+			event.preventDefault();
+			var hash = this.hash;
+
+			$("html, body").animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+				window.location.hash = hash;
+			});
+		}
+	});
+
+	$("#e-mail").on("click", function(event){
+		console.log("email clicked");
+		
+	});
+
+	// opens github page
+	$(".linkedIn-btn").on("click", function(){
+		window.open("http://www.linkedin.com/in/jin-redmond");
+	});
+
+	// opens linked in page
+	$(".github-btn").on("click", function(){
+		window.open("http://github.com/jinredhub");
+	});
+
+	// opens first portfolio page 
+	$("#link-portfolio1").on("click", function(){
+		window.open("https://vrooms.github.io/entrance/");
+	});
+	// opens second portfolio page
+	$("#link-portfolio2").on("click", function(){
+		window.open("https://nba-app-fun.herokuapp.com/stats.html");
+	});
+	// opens third portfolio page
+	$("#link-portfolio3").on("click", function(){
+		window.open("https://the-generators.github.io/whereAreMyFriends/");
+	});
 })
 
-// opens github page
-$(".linkedIn-btn").on("click", function(){
-	window.open("http://www.linkedin.com/in/jin-redmond");
-});
-
-// opens linked in page
-$(".github-btn").on("click", function(){
-	window.open("http://github.com/jinredhub");
-});
-
-// opens first portfolio page 
-$("#link-portfolio1").on("click", function(){
-	window.open("https://vrooms.github.io/entrance/");
-});
-// opens second portfolio page
-$("#link-portfolio2").on("click", function(){
-	window.open("https://nba-app-fun.herokuapp.com/stats.html");
-});
-// opens third portfolio page
-$("#link-portfolio3").on("click", function(){
-	window.open("https://the-generators.github.io/whereAreMyFriends/");
-});
 
 
 
