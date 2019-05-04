79914ededc65320d46d2d16cdbf6c91ed4c0145d
jinredhub <jin@nieblo.com>
2019-05-03 23:34:42 -0700

    updated website

--- js/main.js
+++ js/main.js
@@ -1,55 +1,42 @@
 $(document).ready(function(){
 
-	$("a").on("click", function(event){
-		if(this.hash !== ""){
-			event.preventDefault();
-			var hash = this.hash;
+    $('.visitPinterestButton').on('click', function(){
+        window.location.href = "https://sprung-pinterest.herokuapp.com/";
+    });
+    $('.visitVroomButton').on('click', function(){
+        window.location.href = "https://vrooms.github.io/entrance/";
+    });
+    $('.visitInstaAPIButton').on('click', function(){
+        window.location.href = "https://the-generators.github.io/whereAreMyFriends/";
+    });
 
-			$("html, body").animate({
-				scrollTop: $(hash).offset().top
-			}, 800, function(){
-				window.location.hash = hash;
-			});
-		}
-	});
+    $('.linkedin').on('click', function(){
+        window.location.href = "http://www.linkedin.com/in/jin-redmond";
+    });
+    $('.github').on('click', function(){
+        window.location.href = "http://github.com/jinredhub";
+    })
 
-	$("#e-mail").on("click", function(event){
-		// console.log("email clicked");
-		
-	});
+    // smooth scrolling
+    $('a[href^="#"]').on("click", function(event){
+        if(this.hash !== ""){
+            // event.preventDefault();
+            var hash = this.hash;
 
-	// opens github page
-	$(".linkedIn-btn").on("click", function(){
-		window.open("http://www.linkedin.com/in/jin-redmond");
-	});
+            $("html, body").stop().animate({
+                scrollTop: $(hash).offset().top-57
+            }, 500, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
 
-	// opens linked in page
-	$(".github-btn").on("click", function(){
-		window.open("http://github.com/jinredhub");
-	});
-
-	// opens first portfolio page 
-	$("#link-portfolio1").on("click", function(){
-		window.open("https://vrooms.github.io/entrance/");
-	});
-	// opens second portfolio page
-	$("#link-portfolio2").on("click", function(){
-		window.open("https://nba-app-fun.herokuapp.com/stats.html");
-	});
-	// opens third portfolio page
-	$("#link-portfolio3").on("click", function(){
-		window.open("https://the-generators.github.io/whereAreMyFriends/");
-	});
-	// initialize wow.js scroll animation
+    // initialize wow.js scroll animation
 	new WOW({
 		mobile: false,
 		offset: 50
 	}).init();
 
-
-})
-
-
-
+});
 
 
